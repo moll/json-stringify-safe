bcea2cc804e5da59b28b0bd630b7c7e0989f29dd
Mikeal Rogers <mikeal.rogers@gmail.com>
2013-02-19 11:18:34 -0800

    Expose serialization function.

--- test.js
+++ test.js
@@ -16,4 +16,7 @@ var assert = require('assert');
 assert.equal(JSON.stringify(testObj, null, 2),
              stringify(circularObj, null, 2));
 
+assert.equal(JSON.stringify(testObj, null, 2),
+            JSON.stringify(circularObj, stringify.getSerialize(), 2));
+
 console.log('ok');
