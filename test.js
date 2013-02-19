3d24f1ab2a73c9a96bd38644470c75ef0062f4a1
isaacs <i@izs.me>
2013-02-19 11:00:26 -0800

    test

---
+++ test.js
@@ -0,0 +1,19 @@
+var stringify = require('./stringify.js');
+
+var circularObj = {};
+circularObj.circularRef = circularObj;
+circularObj.list = [ circularObj, circularObj ];
+
+var testObj = {
+  "circularRef": "[Circular]",
+  "list": [
+    "[Circular]",
+    "[Circular]"
+  ]
+};
+
+var assert = require('assert');
+assert.equal(JSON.stringify(testObj, null, 2),
+             stringify(circularObj, null, 2));
+
+console.log('ok');
